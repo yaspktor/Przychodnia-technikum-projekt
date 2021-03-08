ef59ce70e59485c430c153a20b282f86d3a0a671
Yastor0001 <65192722+Yastor-py@users.noreply.github.com>
2021-03-08 22:25:50 +0100

    css

--- login.php
+++ login.php
@@ -15,16 +15,51 @@
         $database = 'przychodnia';
         $connect = mysqli_connect($host, $user, $password, $database);
     ?>
-
-    <a href="./index.php">Strona startowa</a><br>
-    <a href="./register.php">Rejestracja</a>
-
+    <div id="container">
+    
+    <div id="form-css">
+    <!--  
     <form action="./login.php" method="POST">
         <label for="login">Podaj login: </label><input type="text" name="login" id="login" required><br>
         <label for="password">Podaj hasło: </label><input type="password" name="password" id="password" required><br>
-        <input type="submit" value="Zatwierdź">
-    </form>
+        <input type="submit" value="Zatwierdź" id="but1" class="btn btn-primary btn-ghost">
 
+        </form>
+-->
+
+        <div class="signupSection">
+  <div class="info">
+    <h2>Zadbaj o swoją przyszłość</h2>
+    <i class="icon ion-ios-ionic-outline" aria-hidden="true"></i>
+    <p>Zbadaj się już teraz</p>
+  </div>
+  <form action="./login.php" method="POST" class="signupForm" name="signupform">
+    <h2>Zaloguj się</h2>
+    <ul class="noBullet">
+      <li>
+        <label for="username"></label>
+        <input type="text" class="inputFields" id="username" name="username" placeholder="Username" value="" required/>
+      </li>
+      <li>
+        <label for="password"></label>
+        <input type="password" class="inputFields" id="password" name="password" placeholder="Password" value=""  required/>
+      </li>
+      
+      <li id="center-btn">
+        <input type="submit" id="but1" name="zaloguj" alt="Zaloguj" value="Zaloguj">
+      </li>
+      <li id="links">
+        <a  class="linki" href="./register.php">Rejestracja</a><br><a  class="linki" href="./index.php">Strona startowa</a>
+      </li>
+    </ul>
+  </form>
+</div>
+
+
+
+   
+    </div>
+    </div>
     <?php
         function correctLogin($connect, $login) {
             $query = "SELECT `login` from users;";
