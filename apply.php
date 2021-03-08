828d197267754933e3bbfc4e41e7ff47652eb22c
Yastor0001 <65192722+Yastor-py@users.noreply.github.com>
2021-03-08 21:19:13 +0100

    css

--- apply.php
+++ apply.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zapisz się!</title>
+    
 </head>
 <body>
     <?php #połączenie z bazą danych testtest
@@ -14,7 +16,7 @@
         $database = 'przychodnia';
         $connect = mysqli_connect($host, $user, $password, $database);
     ?>
-
+    <div id="container">
     <a href="./index.php">Wyloguj się</a><br>
     <a href="./user.php">Wróć do strony pacjenta!</a>
 
@@ -24,7 +26,7 @@
     <form action="evening.php" method="POST">
         <input type="submit" value="Po 14:20">
     </form>
-
+    </div>
     <?php
 
         function toTwo($value) {
